d7660081e1cdd60c0b584ab69375fc0292349986
sairoutine <sairoutine@gmail.com>
2016-01-24 18:23:09 +0900

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,54 @@
 var m = require('mithril');
 
-//カウンター
-var counter = 0;
+var Question = function() {
+	return m.request({method: "GET", url: "/question"});
+};
 
-//タイマーでカウントアップ
-setInterval(function () {
-    counter++;
-    m.redraw(true);
-}, 1000);
 
-//ビュー
-function view() {
-	return {tag: "a", attrs: {href:"#"}, children: ["count: ", counter]};
-}
+var Introduction = {
+	controller: function() {},
+	view: function() {
+		return {tag: "div", attrs: {}, children: [
+			"Pixivの東方プロジェクトの絵がランダムに表示されます。", {tag: "br", attrs: {}}, 
+			"タグに「ロングスカート」がついているか当ててみましょう。", {tag: "br", attrs: {}}, 
+			{tag: "a", attrs: {href:"/start", config:m.route}, children: ["Start"]}
+		]};
+	}
+};
 
-//HTML要素にコンポーネントをマウント
-m.mount(document.body, {view: view});
+var Start = {
+	controller: function() {
+		this.model = new Question();
+	},
+	view: function(ctrl) {
+		this.answer = function(e) {
+			e.preventDefault();
 
+			var user_answer = e.target.getAttribute('data');
+			var is_long_skirt = ctrl.model().is_long_skirt;
+			if(is_long_skirt == user_answer){
+				alert('正解');
+			}
+			else{
+				alert('不正解');
+			}
+			m.route('/start');
+		};
+
+		return {tag: "div", attrs: {}, children: [
+			"ロングスカートタグが", {tag: "br", attrs: {}}, 
+			{tag: "a", attrs: {onclick:this.answer, href:"#", data:"1"}, children: ["ついている"]}, "　", 
+			{tag: "a", attrs: {onclick:this.answer, href:"#", data:"0"}, children: ["ついていない"]}, {tag: "br", attrs: {}}, 
+			{tag: "br", attrs: {}}, 
+			{tag: "img", attrs: {src: "/image?url=" + ctrl.model().illust_url}}, {tag: "br", attrs: {}}
+		]};
+	},
+
+};
+
+
+
+m.route(document.getElementById("root"), "/", {
+	"/": Introduction,
+	"/start": Start,
+});
